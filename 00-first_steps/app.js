3120cdfd25851c8e48bb6c47bdd582de80a4faf5
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-07-06 11:27:46 +0100

    ng-controller

--- 00-first_steps/app.js
+++ 00-first_steps/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp',[]);
+
+app.controller('nameController', function($scope){
+    $scope.firstName = "Itachi";
+    $scope.lastName = "Uchiha";
+
+    $scope.printName = function(){
+        return $scope.firstName + ' ' + $scope.lastName;
+    }
+})
