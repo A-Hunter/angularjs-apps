dc46c490982139b0bf241ce924cce69c2e1aa076
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-07-08 18:33:15 +0100

    ng-options

--- 02-forms/controllers/controller.js
+++ 02-forms/controllers/controller.js
@@ -1,6 +1,12 @@
 angular.module('eventApp').controller('mainCtrl',['eventFactory', function(eventFactory){
     this.event = eventFactory.getAllEvents();
 
+    this.categories = [{id:1, name:'Music',group:'Primary'},
+                       {id:2, name:'Cinema',group:'Primary'},
+                       {id:3, name:'Video Games',group:'Secondary'},
+                       {id:4, name:'Sport',group:'Secondary'},
+                       {id:5, name:'Reading',group:'Secondary'}];
+
     this.submitForm =  function(form){
         eventFactory.createEvent(angular.copy(form), this.event);
         console.log(this.event);
