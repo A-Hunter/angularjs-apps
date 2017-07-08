5c07c27573fb8b07255d55dc92fcab6228a44d0e
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-07-08 15:07:50 +0100

    accessing objects without global variables

--- 02-forms/controllers/controller.js
+++ 02-forms/controllers/controller.js
@@ -1,5 +1,5 @@
 angular.module('eventApp').controller('mainCtrl',['$scope','eventFactory', function($scope,eventFactory){
-    $scope.event = [];
+    $scope.event = eventFactory.getAllEvents();
 
     $scope.submitForm =  function(form){
         eventFactory.createEvent(angular.copy(form), $scope.event);
--- 02-forms/services/services.js
+++ 02-forms/services/services.js
@@ -2,10 +2,17 @@ angular.module('eventApp').factory('eventFactory', function(){
 
     var eventFactory = {};
 
+    var events =[]; // used for saving data even when changing the scope
+
     eventFactory.createEvent = function(event, eventList){
-        eventList.push(event);
+//        eventList.push(event); // used for saving data even when changing the scope
+        events.push(event);
+        eventList = events;
         return eventList;
     }
 
+    eventFactory.getAllEvents = function(){
+        return events;
+    }
     return eventFactory;
 })
